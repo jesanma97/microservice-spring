689c9f74f7230604f427438142b32776282a1a13
Jesus Enrique Sangonzalo Martinez <je.sangonzalo@soprasteria.com>
2024-05-02 11:18:57 +0200

    First microservice customer done with the operation of saving data in a PostGreSQL DB with docker

---
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.example.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        //TODO: check if email valid
+        //TODO: check if email not taken
+        customerRepository.save(customer);
+    }
+}
