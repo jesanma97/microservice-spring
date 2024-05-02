81c8905deaa645f9a45219186c916317925b1e90
Jesus Enrique Sangonzalo Martinez <je.sangonzalo@soprasteria.com>
2024-05-02 14:35:09 +0200

    Implementation of rabbitmq to control de messages to save a notification

---
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -0,0 +1,21 @@
+package com.example.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.example.notification",
+                "com.example.amqp",
+        }
+)
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
