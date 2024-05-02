a80071961897434a1dd201ceb73939837caf4f3e
Jesus Enrique Sangonzalo Martinez <je.sangonzalo@soprasteria.com>
2024-05-02 12:08:33 +0200

    Second microservice for checking if a customer is fraudulent

--- customer/src/main/java/com/example/customer/CustomerController.java
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -1,6 +1,7 @@
 package com.example.customer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new customer registration {}", customerRegistrationRequest);
--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -1,9 +1,19 @@
 package com.example.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate){
+        this.customerRepository = customerRepository;
+        this.restTemplate = restTemplate;
+    }
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -12,6 +22,18 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         //TODO: check if email valid
         //TODO: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        //TODO: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                    FraudCheckResponse.class,
+                    customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+
+        //TODO: send notification
     }
 }
